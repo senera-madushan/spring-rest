82e9d2c147fd02fb340bbeddfb7504383c29ff45
senera-madushan <it16235298@my.sliit.lk>
2020-03-29 21:58:01 +0530

    implemented the employee repository.

---
+++ src/main/java/com/sliit/springrest/payroll/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.sliit.springrest.payroll;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+
+
+}
